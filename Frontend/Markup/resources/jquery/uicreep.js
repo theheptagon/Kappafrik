7ce3739014be05464904a32f0308d59fd7b14200
sakasif <sak_asif@live.com>
2015-12-20 23:41:31 +0500

    responsive fix

--- Frontend/Markup/resources/jquery/uicreep.js
+++ Frontend/Markup/resources/jquery/uicreep.js
@@ -35,17 +35,6 @@ var swipers = new Swiper('.section-thumb .swiper-container', {
     speed:500
 });
 
-// slider for partners
-// -------------------------------------------
-var swiper = new Swiper('.partners-listing .swiper-container', {
-        pagination: '.partners-listing .swiper-pagination',
-        slidesPerView: 5,
-        paginationClickable: true,
-        spaceBetween: 0,
-        // Navigation arrows
-        nextButton: '.partners-listing .swiper-button-next',
-        prevButton: '.partners-listing .swiper-button-prev'
-    });
 
 
 // small fix for tabs to remove active class from siblings 
@@ -63,3 +52,40 @@ $('.nav-tabs li a').click(function(){
 //        navigation.removeClass('right'); 
 //    }
 //});
+
+
+
+// responsvie utility 
+// -------------------------------------------
+var windw = $(window).width();
+if (windw <= 1024 && windw > 480) {
+    var swiper = new Swiper('.partners-listing .swiper-container', {
+        pagination: '.partners-listing .swiper-pagination',
+        slidesPerView: 3,
+        paginationClickable: true,
+        spaceBetween: 0,
+        // Navigation arrows
+        nextButton: '.partners-listing .swiper-button-next',
+        prevButton: '.partners-listing .swiper-button-prev'
+    });
+} else if (windw > 1024) {
+    var swiper = new Swiper('.partners-listing .swiper-container', {
+        pagination: '.partners-listing .swiper-pagination',
+        slidesPerView: 5,
+        paginationClickable: true,
+        spaceBetween: 0,
+        // Navigation arrows
+        nextButton: '.partners-listing .swiper-button-next',
+        prevButton: '.partners-listing .swiper-button-prev'
+    });
+} else if (windw <= 480) {
+    var swiper = new Swiper('.partners-listing .swiper-container', {
+        pagination: '.partners-listing .swiper-pagination',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 0,
+        // Navigation arrows
+        nextButton: '.partners-listing .swiper-button-next',
+        prevButton: '.partners-listing .swiper-button-prev'
+    });
+}
